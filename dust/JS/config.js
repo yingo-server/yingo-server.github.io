b4bc514c437f64d21b763a74ef9fba58f78008c4
yingo-server <18674211622@163.com>
2026-02-19 21:14:04 +0800

    Upload folder: config.js

---
+++ dust/JS/config.js
@@ -0,0 +1,7 @@
+// Gitee API 配置
+var GITEE_CONFIG = {
+    ACCESS_TOKEN: 'e823c3a1265e30f429305ddebc27f855',
+    OWNER: 'yingo-server',
+    REPO: 'dust-light',
+    BASE_URL: 'https://gitee.com/api/v5'
+};
