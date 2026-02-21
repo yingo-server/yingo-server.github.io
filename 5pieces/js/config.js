cb3ba3c46119d70d7b3ba3e24d4dc26b6c1c0f02
yingo-server <18674211622@163.com>
2026-02-22 01:25:53 +0800

    Upload folder: config.js

---
+++ 5pieces/js/config.js
@@ -0,0 +1,9 @@
+// js/config.js - 改为普通对象（不使用 export）
+window.GITEE_CONFIG = {
+    owner: 'yingo-server',
+    repo: 'five-pie',
+    token: 'e823c3a1265e30f429305ddebc27f855',
+    branch: 'master'
+};
+
+window.POLL_INTERVAL = 2000;
