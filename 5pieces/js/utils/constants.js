a549d8375bee481fc2dc30d844c66f0daa4668ff
yingo-server <18674211622@163.com>
2026-02-24 01:06:10 +0800

    Upload folder: constants.js

---
+++ 5pieces/js/utils/constants.js
@@ -0,0 +1,7 @@
+// 游戏常量
+export const BOARD_SIZE = 15;
+export const CANVAS_SIZE = 600;
+export const MARGIN = 30;
+export const CELL_SIZE = (CANVAS_SIZE - 2 * MARGIN) / (BOARD_SIZE - 1);
+export const PIECE_RADIUS = CELL_SIZE * 0.42;
+export const ANIMATION_DURATION = 150; // 毫秒
